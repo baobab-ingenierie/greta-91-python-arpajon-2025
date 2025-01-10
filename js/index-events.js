ddbb177d519aea207bcc30bc6c250ff00ab93f33
lesly-lodin <lesly.lodin@baobab-ingenierie.fr>
2025-01-10 14:58:12 +0100

    Web : Day 5

--- js/index-events.js
+++ js/index-events.js
@@ -30,3 +30,43 @@ document.querySelector('#langue').addEventListener(
         // Remplit liste pays
     }
 );
+
+/**
+ * Branche l'écouteur d'événement CLICK sur l'élément SELECTALL
+ */
+document.querySelector('#selectAll').addEventListener(
+    'click',
+    function () {
+        // Tableau des cases à cocher
+        // let aChecks = document.querySelectorAll('form fieldset:last-of-type input[type=checkbox][id^=pow]');
+        let aChecks = document.querySelectorAll('form fieldset:last-of-type input[name]');
+        // console.log(aChecks); // Vérif
+
+        // Coche/Décoche chaque élément du tableau
+        aChecks.forEach(elt => {
+            elt.checked = this.checked;
+        });
+    }
+);
+
+/**
+ * Gestion dark/light mode
+ */
+document.querySelector('#darkMode').addEventListener(
+    'click',
+    function () {
+        // document.body.style.backgroundColor = 'black';
+        // document.body.style.color = 'white';
+        document.body.setAttribute('data-bs-theme', 'dark')
+        document.body.style.transition = 'all 1s';
+    }
+);
+
+document.querySelector('#lightMode').addEventListener(
+    'click',
+    function () {
+        // document.body.style.backgroundColor = 'white';
+        // document.body.style.color = 'black';
+        document.body.setAttribute('data-bs-theme', 'light')
+    }
+);
